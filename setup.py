1924bebe4c61ed4a67f25da13ff1943e3612acd8
Najam Syed <najam.r.syed@gmail.com>
2019-04-30 19:20:40 -0400

    Fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="shapeutil",
     version="0.1.0",
-    description="Utility for obtain coordinates for various shapes",
+    description="Utility for obtaining coordinates for various shapes",
     url="https://github.com/nrsyed/shape-util",
     author="Najam R Syed",
     author_email="najam.r.syed@gmail.com",
